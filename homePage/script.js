8cffcc902b0cda62e39cc1219788b643aa922a52
mahmoud-Riad00 <faydmhmwd1@gmail.com>
2024-09-05 02:15:45 +0300

    s

--- homePage/script.js
+++ homePage/script.js
@@ -48,7 +48,7 @@ function updateMainimg(){
     categoryDescription.innerHTML=`
     <h1>${data[currentindex].name}</h1>
     <p>${descriptions[currentindex].description}</p>
-    <div class="cat-product" > <a href="https://api.escuelajs.co/api/v1/categories/${data[currentindex].id}/products">More Details</a></div>`
+    <div class="cat-product" > <a href="https://api.escuelajs.co/api/v1/categories/${data[currentindex].id}/products">Buy Now</a></div>`
 }
 
 function autoSlid(){
